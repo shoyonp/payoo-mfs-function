8c0ea71eceb251b422e464d5c95a00ee357c1d4b
shoyonp <shoyonkumar143@gmail.com>
2024-09-19 23:23:16 +0600

    utilitise file added

---
+++ js/addMoney.js
@@ -0,0 +1,16 @@
+/**
+ * how to get a number from an input field
+ * 1. creat a variable 
+ * 2. right side document.getElementById(id)
+ * 3. value
+ * 4. parseFloat
+ */
+
+console.log('add money loaded');
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+
+    console.log('add money button clikced');
+    // const addMoney = document.getElementById('input-add-money').value;
+    // const addMoneyNumber = parseFloat(addMoney);
+})
---
+++ js/utilities.js
@@ -0,0 +1,9 @@
+/**
+ * common shared functions here
+ */
+console.log('utilities loaded');
+
+
+function getInputFieldValueById (){
+    console.log('will get value by id');
+}
