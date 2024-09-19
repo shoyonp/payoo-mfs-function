92590d19306be535a8344f4b3bef672dd67f0700
shoyonp <shoyonkumar143@gmail.com>
2024-09-20 03:13:58 +0600

    transaction history on progress

--- js/features.js
+++ js/features.js
@@ -1,4 +1,11 @@
 document.getElementById('show-add-money-form').addEventListener('click',function(){
-    console.log('show add money button clicked');
-    showSectionById('add-money-form')
+    showSectionById('add-money-form');
+});
+
+document.getElementById('show-cash-out-form').addEventListener('click',function(){
+    showSectionById('cash-out-form');
+})
+
+document.getElementById('show-transaction-histoy').addEventListener('click',function(){
+    showSectionById('transaction-section');
 })
