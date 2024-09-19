a556d97bfe9430fcef2f9d11c51f945f65c24920
shoyonp <shoyonkumar143@gmail.com>
2024-09-19 23:43:19 +0600

    using separate files

--- js/addMoney.js
+++ js/addMoney.js
@@ -6,11 +6,19 @@
  * 4. parseFloat
  */
 
-console.log('add money loaded');
+// console.log('add money loaded');
 document.getElementById('btn-add-money').addEventListener('click',function(event){
     event.preventDefault();
 
     console.log('add money button clikced');
+
+    // getInputFieldValueById();
+    // const addMoney = getInputFieldValueById();
+    // console.log('add money value', addMoney);
     // const addMoney = document.getElementById('input-add-money').value;
     // const addMoneyNumber = parseFloat(addMoney);
+
+    const addMoney = getInputFieldValueById('input-add-money');
+    const pinNumber = getInputFieldValueById('input-pin-number');
+    console.log('add money with parameter', addMoney,pinNumber);
 })
--- js/utilities.js
+++ js/utilities.js
@@ -1,9 +1,16 @@
 /**
  * common shared functions here
  */
-console.log('utilities loaded');
+// console.log('utilities loaded');
 
 
-function getInputFieldValueById (){
-    console.log('will get value by id');
+// function getInputFieldValueById (){
+//     console.log('will get value by id');
+//     const addMoney = document.getElementById('input-add-money').value;
+//     return addMoney;
+// }
+
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    return inputValue;
 }
